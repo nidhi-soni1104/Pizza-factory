e21f63d85491331222f866b1e6bd6535df99b390
Nidhi Soni <nidhisoni4411@gmail.com>
2025-02-03 11:32:02 +0530

    fixed failed test

--- test/controllers/orders_controller_test.rb
+++ test/controllers/orders_controller_test.rb
@@ -1,25 +1,34 @@
 require "test_helper"
 
 class OrdersControllerTest < ActionDispatch::IntegrationTest
-  setup do
-    @veg_pizza = PizzaMenu.create!(
-      name: "Deluxe Veggie",
-      category: "Vegetarian",
-      price_regular: 150,
-      price_medium: 200,
-      price_large: 325
-    )
-    @nonveg_pizza = PizzaMenu.create!(
-      name: "Non-Veg Supreme",
-      category: "Non-Vegetarian",
-      price_regular: 190,
-      price_medium: 325,
-      price_large: 425
-    )
-    @topping1 = Topping.create!(name: "Black olive", topping_type: "veg", price: 20)
-    @topping2 = Topping.create!(name: "Chicken tikka", topping_type: "nonveg", price: 35)
-    @side = Side.create!(name: "Cold drink", price: 55)
-  end
+	setup do
+	  @veg_pizza = PizzaMenu.create!(
+	    name: "Deluxe Veggie",
+	    category: "Vegetarian",
+	    price_regular: 150,
+	    price_medium: 200,
+	    price_large: 325
+	  )
+	  @nonveg_pizza = PizzaMenu.create!(
+	    name: "Non-Veg Supreme",
+	    category: "Non-Vegetarian",
+	    price_regular: 190,
+	    price_medium: 325,
+	    price_large: 425
+	  )
+	  @topping1 = Topping.create!(name: "Black olive", topping_type: "veg", price: 20)
+	  @topping2 = Topping.create!(name: "Chicken tikka", topping_type: "nonveg", price: 35)
+	  @side = Side.create!(name: "Cold drink", price: 55)
+
+	  # Create Inventory records needed for the test
+	  Inventory.create!(item_type: "pizza", item_name: @veg_pizza.name, quantity: 50)
+	  Inventory.create!(item_type: "pizza", item_name: @nonveg_pizza.name, quantity: 50)
+	  # If needed, also create Inventory for toppings and sides
+	  Inventory.create!(item_type: "topping", item_name: @topping1.name, quantity: 100)
+	  Inventory.create!(item_type: "topping", item_name: @topping2.name, quantity: 100)
+	  Inventory.create!(item_type: "side", item_name: @side.name, quantity: 100)
+	end
+
 
   test "should create a valid vegetarian order" do
     order_params = {
